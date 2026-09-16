c2426c55fd4b76194820b97093847de8212dfc00
Robert Eisenman <robert.t.eisenman@gmail.com>
2026-09-16 08:58:37 -0700

    list feeds in better format

--- handler_feed.go
+++ handler_feed.go
@@ -33,19 +33,19 @@ func handlerAddFeed(s *state, cmd command) error {
 		return fmt.Errorf("Problem creating new feed: %w", err)
 	}
 	fmt.Println("New feed record created successfully")
-	printFeed(feed)
+	printFeed(feed, currentUser)
 	fmt.Println()
 	fmt.Println("=====================================")
 	return nil
 }
 
-func printFeed(feed database.Feed) {
+func printFeed(feed database.Feed, user database.User) {
 	fmt.Printf("* ID:            %s\n", feed.ID)
 	fmt.Printf("* Created:       %v\n", feed.CreatedAt)
 	fmt.Printf("* Updated:       %v\n", feed.UpdatedAt)
 	fmt.Printf("* Name:          %s\n", feed.Name)
 	fmt.Printf("* URL:           %s\n", feed.Url)
-	fmt.Printf("* UserID:        %s\n", feed.UserID)
+	fmt.Printf("* User:          %s\n", user.Name)
 }
 
 func handlerListFeeds(s *state, cmd command) error {
@@ -56,18 +56,20 @@ func handlerListFeeds(s *state, cmd command) error {
 	if err != nil {
 		return fmt.Errorf("Error retrieving feeds: %w", err)
 	}
-	fmt.Println("Feeds List:")
-	for i, feed := range feeds {
-		fmt.Printf("Feed %d\n", i+1)
-		fmt.Printf("* Name:          %s\n", feed.Name)
-		fmt.Printf("* URL:           %s\n", feed.Url)
+
+	if len(feeds) == 0 {
+		fmt.Println("No feeds found.")
+		return nil
+	}
+
+	fmt.Printf("Found %d feeds:\n", len(feeds))
+	for _, feed := range feeds {
 		user, err := s.db.GetUserByID(context.Background(), feed.UserID)
 		if err != nil {
 			return fmt.Errorf("Couldn't retrieve user: %w", err)
 		}
-		userName := user.Name
-		fmt.Printf("* User Name:     %s\n", userName)
-		fmt.Println("========")
+		printFeed(feed, user)
+		fmt.Println("=====================================")
 	}
 	return nil
 }
